42a7161a15e9fa3d93f5699254299c8836d1ee3b
Mahima Sharma <155097110+Mahima-033@users.noreply.github.com>
2026-02-27 16:46:57 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+const header=document.getElementsByClassName("header-el")[0];
+const input=document.querySelector("#text");
+const button=document.querySelector(".btn");
+
+button.addEventListener("click",function () {
+    let name= input.value;
+    header.textContent="Hello, " + name;
+});
+
+const red=document.querySelector(".red");
+red.addEventListener("click",function(){
+    red.style.backgroundColor = "red";
+    red.style.color = "white";
+})
+
+const blue=document.querySelector(".blue");
+blue.addEventListener("click",function(){
+    blue.style.backgroundColor = "blue";
+    blue.style.color = "white";
+})
+
+const green=document.querySelector(".green");
+green.addEventListener("click",function(){
+    green.style.backgroundColor = "green";
+    green.style.color = "white";
+
+})
+
+const yellow=document.querySelector(".yellow");
+yellow.addEventListener("click",function(){
+    yellow.style.backgroundColor = "yellow";
+    yellow.style.color = "white";
+})
+
+
+
